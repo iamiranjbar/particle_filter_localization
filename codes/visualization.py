69cdf871bbe2435796921a8ab42d2e8dfc0d71df
Amir Ranjbar <iamirranjbar@gmail.com>
2021-07-21 00:18:28 +0430

    Fix visualization bug.

--- codes/visualization.py
+++ codes/visualization.py
@@ -25,10 +25,10 @@ def draw_status(point, color):
     tetha = point[2]
     delta_x = 0.00001 * math.cos(tetha)
     delta_y = 0.00001 * math.sin(tetha)
-    plt.arrow(x, y, delta_x, delta_y , head_width = 0.02, fill=False, overhang = 0.6, color=color)
+    plt.arrow(y, x, delta_y, delta_x , head_width = 0.02, fill=False, overhang = 0.6, color=color)
 
 
 def draw_dot_line(start_point, end_point, color):
     start_x, start_y = start_point[0], start_point[1]
     end_x, end_y = end_point[0], end_point[1]
-    plt.plot([start_x, end_x], [start_y, end_y], color=color, linestyle=':')
+    plt.plot([start_y, end_y], [start_x, end_x], color=color, linestyle=':')
